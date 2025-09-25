1863b69e5a9ef14d89323451b374778d6ab6a12e
Vader <thesecondshot7@gmail.com>
2025-09-24 20:40:19 -0700

    id added for better filtering

--- src/Movies/Movie.jsx
+++ src/Movies/Movie.jsx
@@ -37,13 +37,11 @@ function Movie({ films, series, showWatchList }) {
         <div class="p-[20px] h-[calc(100vh-80px)] overflow-auto">
             <header class='flex justify-center gap-[20px] p-[10px]  mb-[20px]'>
                 <button>All Films</button>
-                <button>Series</button>
-                <button>Favourites</button>
-                <button>My Watchlist</button>
+               
             </header>
             <Favourites favourites={favourites} setFavourites={setFavourites} />
             <div class='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4'>{
-                showWatchList ? <Watchlist watchList={watchList} setWatchList={setWatchList} /> : filmDisplay.map((film) => (<div key={film.Title}>
+                showWatchList ? <Watchlist watchList={watchList} setWatchList={setWatchList} /> : filmDisplay.map((film) => (<div key={film.id}>
                     <img src={film.Poster} alt={film.Title} />
                     <h2>{film.Title}</h2>
                     <p>{film.Year}</p>
