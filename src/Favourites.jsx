025feefa286a09587e73a4c8e728900fe0b7433a
Vader <thesecondshot7@gmail.com>
2025-09-27 06:55:01 -0700

    omo

--- src/Favourites.jsx
+++ src/Favourites.jsx
@@ -1,26 +1,3 @@
-/* import { X } from "lucide-react"
-function Favourites({favourites,setFavourites}){
- console.log(favourites)
-
-   function removeFavourites(id){
-      setFavourites( favourites.filter((film) => film.id !== id))
-   }
-
-   
-
-    return(
-        <div class='flex gap-4 p-4 h-[150px] mb-[20px] border-b-[1px] border-gray-300 pb-[20px]'>
-            {favourites.map((film)=>(<div key={film.id}>
-                    <img src={film.poster} alt={film.movie} class='h-[80%]' />
-                    <h2>{film.movie}</h2>
-                    <p>{film.year}</p>
-                    <button onClick={()=>removeFavourites(film.id)}><X /></button>
-                </div>))}
-        </div>
-    )
-}
-
-export default Favourites */
 
 import { useEffect } from "react";
 import { X } from "lucide-react"
